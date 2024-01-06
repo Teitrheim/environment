bf336a6b5363d53d569de08d1153242d56c3a6da
Thomas Eitrheim <teitrheim@gmail.com>
2024-01-06 18:12:26 +0100

    updated the function for generating a random Dad joke to return a promise.

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,5 +1,7 @@
-import "example-joker";
-
 import joker from "give-me-a-joke";
 
-joker.getRandomDadJoke(console.log);
+export function dadJoke() {
+  return new Promise(function (resolve, reject) {
+    joker.getRandomDadJoke(resolve);
+  });
+}
